c091f26188e2aff027b94e7e86ad686eeca5eff0
Konrad Gruzlo <gruzlokonrad@gmail.com>
2022-07-01 13:35:09 +0200

    add event of add active class

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const titleClickHandler = function (event) {
-  console.log('Link was clicked!');
-  console.log(event);
+  const clickedElement = this;
+  console.log('current target: ', event.currentTarget);
+  console.log('target: ', event.target);
 
   /* remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -11,6 +12,9 @@ const titleClickHandler = function (event) {
   }
   
   /* add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+
   
   /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('article.active');
